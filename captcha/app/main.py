a63ccc0cca42f6052832c1b9b4428b3acb1762a6
Ng Jun Kang <ng_jun_kang@hotmail.com>
2021-07-12 23:34:38 +0800

    added uvicorn for running the server

--- captcha/app/main.py
+++ captcha/app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI, Response
 from captcha_solver import solve_captcha
+import uvicorn
 
 app = FastAPI()
-
 
 @app.get('/')
 async def main(url: str):
     return Response(content=solve_captcha(url), media_type='text/plain')
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="0.0.0.0", port=8080)
