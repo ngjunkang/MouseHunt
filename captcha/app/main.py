f8067e1292c043fc6f07c4cd45def6e8bc628fd3
Ng Jun Kang <ng_jun_kang@hotmail.com>
2022-05-17 00:24:59 +0800

    Change port to env var

--- captcha/app/main.py
+++ captcha/app/main.py
@@ -9,4 +9,4 @@ async def main(url: str):
     return Response(content=solve_captcha(url), media_type='text/plain')
 
 if __name__ == '__main__':
-    uvicorn.run("captcha.app.main:app", host="0.0.0.0", port=8080, reload=True, workers=2)
+    uvicorn.run("captcha.app.main:app", host="0.0.0.0", port=os.environ.get('PORT', '5000'), reload=True, workers=2)
