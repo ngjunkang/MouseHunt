bd984f6b7083195afa3e1f972f3df26553a4c7c6
Ng Jun Kang <ng_jun_kang@hotmail.com>
2022-05-17 00:57:49 +0800

    Add env var to tesseract path

--- captcha/app/captcha_solver.py
+++ captcha/app/captcha_solver.py
@@ -1,8 +1,9 @@
 import pytesseract
 import string
+import os
 from image_utils import download_image, preprocess_image
 
-pytesseract.pytesseract.tesseract_cmd = r'/usr/bin/tesseract'
+pytesseract.pytesseract.tesseract_cmd = os.environ.get("TESS_PATH", r'/usr/bin/tesseract')
 
 def solve_captcha(url: str) -> str:
     image = download_image(url)
